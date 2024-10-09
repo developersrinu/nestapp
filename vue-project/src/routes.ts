b1c88b8a53ee0a426eec7f0173cc657814272e86
Srinu <srinu.orsu@logikal.ai>
2024-10-09 14:31:04 +0530

    changes

---
+++ vue-project/src/routes.ts
@@ -0,0 +1,26 @@
+
+import { createRouter, createWebHistory } from 'vue-router';
+import Login from './views/Login.vue'; 
+import Todo from './views/Todo.vue';
+
+const routes = [
+    {
+        path: '/',
+        name: 'Login',
+        component: Login,
+    },
+    {
+        path: '/todo',
+        name: 'Todo',
+        component: Todo,
+    },
+
+
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
